dbf4d12ebb58f0b545d31188c5c6148e2a4ab6cb
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 09:49:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//project=5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message 
+//and store it in a new variable called message. Print your message.
+var famousPerson = "Albert Einstein ";
+var qoute = "a person who never made a mistake never tired anything ";
+var message = ("".concat(famousPerson, " oncesaid, ").concat(qoute));
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,8 @@
+//project=5
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message 
+//and store it in a new variable called message. Print your message.
+
+let famousPerson ="Albert Einstein ";
+let qoute = "a person who never made a mistake never tired anything ";
+let message =(`${famousPerson} oncesaid, ${qoute}`);
+console.log(message);
